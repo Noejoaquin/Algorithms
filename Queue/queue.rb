85aba719b4c12d3a96cb3dcf6f86b246ab1ffeaf
Noe <noe.joaquin.james@gmail.com>
2018-03-06 13:45:15 -0500

    add tests

--- Queue/queue.rb
+++ Queue/queue.rb
@@ -1,15 +1,20 @@
-class Queue
-  def initialize()
+require "minitest/autorun"
+
+
+class MyQueue
+  def initialize
     @queue = []
   end
 
   def enqueue(el)
     @queue.push(el)
+    @queue
   end
 
-  def dequeue(el)
+  def dequeue
     check_queue
-    @queue.unshift
+    @queue.shift
+    @queue
   end
 
   def peek
@@ -24,6 +29,25 @@ class Queue
       raise('queue is empty')
     end
   end
+end
 
+class TestQueue < Minitest::Test
+  def test_enqueue
+    q = MyQueue.new
+    assert_equal [4], q.enqueue(4)
+  end
+
+  def test_dequeue
+    q = MyQueue.new
+    q.enqueue(4)
+    q.enqueue(6)
+    assert_equal [6], q.dequeue
+  end
+
+  def test_peek
+    q = MyQueue.new
+    q.enqueue(4)
+    assert_equal 4, q.peek
+  end
 
 end
