579928955a4671c459722d18e4056f39443fec9c
Noe <noe.joaquin.james@gmail.com>
2018-03-06 13:15:30 -0500

    check for elements in queue

--- Queue/queue.rb
+++ Queue/queue.rb
@@ -8,11 +8,21 @@ class Queue
   end
 
   def dequeue(el)
+    check_queue
     @queue.unshift
   end
 
   def peek
+    check_queue
     @queue[0]
+  end
+
+  private
+
+  def check_queue
+    if @queue.empty?
+      raise('queue is empty')
+    end
   end
 
 
